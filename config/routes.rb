39e8d971c5552826411b7b8fa6ca134095d5cb09
samuraisannn <umetetu@icloud.com>
2021-12-05 10:21:12 +0000

    edit posts view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #ルートurlはpostsコントローラーのindexを呼んでください
-  root to: "posts#index"
+  root to: 'posts#index'
 
 end
